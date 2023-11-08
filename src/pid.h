04bf480ae9a825d9b97bd354504bcf15163eb58d
Yazan Adi <yazanadi2002@hotmail.com>
2023-11-08 02:15:37 +0000

    Updating PID class template

---
+++ src/pid.h
@@ -0,0 +1,21 @@
+
+#include <Arduino.h>
+
+// this #ifndef stops this file
+// from being included mored than
+// once by the compiler. 
+#ifndef _PID_H
+#define _PID_H
+
+// Class to contain generic PID algorithm.
+class PID_c {
+  public:
+  
+    // Constructor, must exist.
+    PID_c() {
+
+    } 
+
+};
+
+#endif
